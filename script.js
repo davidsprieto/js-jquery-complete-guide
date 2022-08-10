62c0a76e16ddc86add6b396c334884a6a93b566f
David Prieto <david.s.prieto1@gmail.com>
2022-08-10 16:59:16 -0500

    Completed Section 2 - #8

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ $(function() {
     // jQuery goes here...
 
     // Uncomment this line to fade out the red box on page load
-    $(".red-box").fadeOut(2000);
+    $(".red-box").fadeTo(3000, 0.2);
+    $(".green-box").fadeTo(2000, 0.5);
+    $(".blue-box").fadeTo(1000, 0.8);
 
+    $(".blue-box").fadeToggle();
+    $(".blue-box").fadeToggle();
 
 });
