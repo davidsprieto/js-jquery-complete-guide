4c33b67fd62a657a9927b8f77d1b41e8005ef979
David Prieto <david.s.prieto1@gmail.com>
2022-08-17 13:40:57 -0500

    Completed Section 3

--- script.js
+++ script.js
@@ -87,3 +87,50 @@ $(function() {
     $(`.lightbox`).delay(500).fadeIn(1000);
 });
  */
+
+// ELEMENT SELECTORS:
+/*
+$(function () {
+    // $(`p`).css(`background-color`, "yellow");
+    // $(`.red-box`).css(`background-color`, "yellow");
+    // $(`#list`).css(`background-color`, `yellow`);
+    // $(`input[type='text']`).css(`background-color`, `yellow`);
+    // $(`input:text`).css(`background-color`, `yellow`);
+    // $(`h2, p, input`).css(`background-color`, `yellow`);
+    // $(`p:first`).css(`background-color`, "yellow");
+    // $(`li:last`).css(`background-color`, "yellow");
+    // $(`li:even`).css(`background-color`, `yellow`);
+    // $(`li:odd`).css(`background-color`, `lightgreen`);
+});
+ */
+
+/*
+$(function() {
+    // $('#list').find('li').css('background-color', 'yellow');
+    // $(`#list`).children(`li`).css(`background-color`, `yellow`);
+    // $(`#list`).parents(`body`).css(`background-color`, `yellow`);
+    // $(`#list`).parent().css(`background-color`, `yellow`);
+    // $(`#list`).siblings(`:header`).css(`background-color`, `yellow`);
+    // $(`#list`).prev().css(`background-color`, `yellow`);
+    // $(`#list`).next().css(`background-color`, `yellow`);
+    // $(`:header`).next().css(`background-color`, `yellow`);
+    // $(`input:password`).css(`background-color`, `yellow`);
+});
+ */
+
+/*
+$(function() {
+    // $(`#list`).children('li').filter(":even").css(`background-color`, `yellow`);
+    // $(`li`).filter(function(index) {
+    //     return index % 3 === 1;
+    // }).css(`background-color`, `yellow`);
+    // $(`li`).first().css(`background-color`, `yellow`);
+    // $(`li`).last().css(`background-color`, `yellow`);
+    // $(`li`).eq(4).css(`background-color`, `yellow`);
+    // $(`li`).eq(-2).css(`background-color`, `yellow`);
+    // $(`li`).not(`:first`).css(`background-color`, `yellow`);
+    // $(`li`).not(function(index) {
+    //     return index % 3 === 0;
+    // }).css(`background-color`, `yellow`);
+});
+ */
